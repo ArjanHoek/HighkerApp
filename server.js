5ca50a4420d7f49de9ddd8502cea713baefaafcb
Arjan Hoek <arjanhoek5191@gmail.com>
2022-03-19 14:26:39 +0100

    make server js ready for deployment

--- server.js
+++ server.js
@@ -8,10 +8,12 @@ import lodgeRouter from './routes/lodges.js';
 
 dotenv.config({ path: './config.env' });
 
-const { DBLINK, DBUSERNAME, DBPASSWORD, PORT } = process.env;
+const { DBLINK, DBUSERNAME, DBPASSWORD } = process.env;
 connect(DBLINK, DBUSERNAME, DBPASSWORD);
 
 const app = express();
+
+app.set('port', process.env.PORT || 5000);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -19,8 +21,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
+app.use(express.static('./client/build'));
 app.use('/lodges', lodgeRouter);
 
-const port = PORT || 5000;
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
-app.listen(port);
+app.listen(app.get('port'));
